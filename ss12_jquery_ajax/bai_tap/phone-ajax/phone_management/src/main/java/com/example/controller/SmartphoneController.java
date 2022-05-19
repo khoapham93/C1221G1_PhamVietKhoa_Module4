1c11290cfa78e2f75f4381b1c161253df4142893
Khoa Pham <vietkhoa93@gmail.com>
2022-05-19 21:54:29 +0700

    jquery

--- ss12_jquery_ajax/bai_tap/phone-ajax/phone_management/src/main/java/com/example/controller/SmartphoneController.java
+++ ss12_jquery_ajax/bai_tap/phone-ajax/phone_management/src/main/java/com/example/controller/SmartphoneController.java
@@ -9,6 +9,7 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.servlet.ModelAndView;
 
+@CrossOrigin
 @RestController
 @RequestMapping("/smartphones")
 public class SmartphoneController {
